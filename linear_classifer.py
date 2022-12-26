c2d1c97ce1ac4b189a30332b3f1827a8cc300d19
Arseniy Kapkov <a.kapkov@digital-core.ru>
2022-12-26 19:26:37 +0300

    working code

--- linear_classifer.py
+++ linear_classifer.py
@@ -51,10 +51,9 @@ def cross_entropy_loss(probs, target_index):
     dims = orig_probs.ndim
     target_probs = orig_probs[range(
         len(orig_probs)), target_index] if dims > 1 else orig_probs[target_index]
-    # print("target arr is \n", target_probs)
     loss_arr = - np.log(target_probs)
-    print("loss array \n", loss_arr)
-    loss = np.average(loss_arr)
+    # print("loss array \n", loss_arr)
+    loss = np.sum(loss_arr)
 
     # print("loss is ", loss)
     return loss
@@ -85,18 +84,11 @@ def softmax_with_cross_entropy(predictions, target_index):
     dim = grad.ndim - 1
 
     target_index = target_index.reshape(-1)
-    # target_index = target_index[:, np.newaxis] if dim > 0 else target_index
-    # print("before grad \n", grad)
     # print("target index\n", target_index)
-    # grad[target_index] -= 1
     if dim > 0:
         grad[range(len(grad)), target_index] -= 1
     else:
         grad[target_index] -= 1
-
-    # for elem, index in zip(grad, target_index):
-    #     # print("index is ", index)
-    #     elem[index] -= 1
 
     # print("grad is \n", grad)
     return loss, grad
@@ -147,17 +139,19 @@ def linear_softmax(X, W, target_index):
     batch_size = target_index.size
     # print("grad is \n", grad)
 
-    batch_dW = np.zeros((batch_size, W.shape[0], W.shape[1]))
-    for k in range(batch_size):
-        dW_ind = np.zeros(W.shape)
-        for i in range(W.shape[0]):
-            for j in range(W.shape[1]):
-                dW_ind[i][j] = X[k][i] * grad[k][j]
+    # batch_dW = np.zeros((batch_size, W.shape[0], W.shape[1]))
+    # dW_ind = np.zeros(W.shape)
+    # for k in range(batch_size):
+    #     for i in range(W.shape[0]):
+    #         for j in range(W.shape[1]):
+    #             dW_ind[i][j] = X[k][i] * grad[k][j]
 
-        batch_dW[k] = dW_ind
+    # batch_dW[k] = dW_ind
+
+    dW = np.dot(X.T, grad)
 
     # print("batch dW is\n ", batch_dW)
-    dW = np.average(batch_dW, axis=0)
+    # dW = np.average(batch_dW, axis=0)
     # print("dW is \n", dW)
 
     # # TODO implement prediction and gradient over W
@@ -215,7 +209,7 @@ class LinearSoftmaxClassifier():
                 self.W -= learning_rate * (dW + reg_dW)
                 loss_history = np.append(loss_history, loss)
                 # if(i % 500 == 0):
-                print("Epoch %i, loss: %f" % (epoch, loss))
+                # print("Epoch %i, loss: %f" % (epoch, loss))
             test_pred = self.predict(X)
             test_pred = np.argmax(test_pred, axis=1)
             test_accuracy = multiclass_accuracy(test_pred, y)*100
